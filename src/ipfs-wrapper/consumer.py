f8268787ebf71d72a24ec40bc8d7f67b96c53596
Nei Cardoso de Oliveira  Neto <nei.neto@hotmail.com>
2021-05-22 02:13:12 -0300

    Add a few comments on ipfs-watcher

--- src/ipfs-wrapper/consumer.py
+++ src/ipfs-wrapper/consumer.py
@@ -4,4 +4,6 @@ Hash = str  # multibase encoded multihash
 
 
 def retrieve(hash: Hash, destination: Path):
+    # could we have a progress bar here?
+    # hash could be a folder with many files or a single file#
     pass
