24c87da998c5b9d4f25e72edbca7f6844c6434ce
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2021-06-22 02:03:06 -0300

    Print results to stdout

--- src/network_chan/strategy.py
+++ src/network_chan/strategy.py
@@ -4,6 +4,6 @@ from . import cli, runner
 def main():
     args = cli.get_args()
     if args.command == "add":
-        runner.add(args.file_path)
+        print(runner.add(args.file_path))
     elif args.command == "get":
-        runner.get(args.url, args.path)
+        print(runner.get(args.url, args.path))
