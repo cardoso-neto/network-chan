f3f54ed1268f6f29042aff792bb04faf8498617b
Nei Cardoso de Oliveira Neto <nei.neto@hotmail.com>
2021-06-22 02:05:12 -0300

    IPFS.get thread straight to its destination path

--- src/network_chan/runner.py
+++ src/network_chan/runner.py
@@ -1,4 +1,3 @@
-import shutil
 from pathlib import Path
 
 from chan_url_lib import ChanURI
@@ -9,14 +8,14 @@ import ipfs_wrapper as ipfs
 
 def add(thread_folder: Path):
     hash_ = ipfs.serve(thread_folder)
-    print(hash_)
     url = str(ChanURI.from_path(thread_folder))
     # dht.put(url, hash_)
+    return hash_
 
 
-def get(url: str, destination_path: Path):
+def get(url: str, destination_path: Path) -> Path:
     thread_url = ChanURI.from_url(url)
-    standardized_url = str(thread_url)
-    # hash_ = dht.get(url)
-    #ipfs.retrieve(hash_, destination_path)
-    # shutil.move(destination_path / hash_, destination_path / str(thread_url.thread_id))
+    # hash_ = dht.get(str(thread_url))
+    thread_folder_path = destination_path / thread_url
+    ipfs.retrieve(hash_, thread_folder_path)
+    return thread_folder_path
